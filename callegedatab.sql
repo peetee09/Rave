89a258ef7a092ac0b524f6986f387b52072cdbb3
peetee09 <93839860+peetee09@users.noreply.github.com>
2021-11-14 23:04:13 +0200

    Add files via upload

---
+++ callegedatab.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE task;    
+USE task;
+CREATE TABLE base(Firstname varchar(14), Surname varchar(14), Alegebra float, Calculus float, Programming float, Datebase float);
+SELECT * FROM base;
